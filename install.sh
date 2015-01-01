846268a0a5eec5d96e11452d62277713304ef5c4
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2015-01-01 22:39:19 +0800

    add misc with some basic aliases and proxychains

--- install.sh
+++ install.sh
@@ -15,14 +15,17 @@ fi
 cp git.sh ~/.git.sh
 cp docker.sh ~/.docker.sh
 cp tmux.sh ~/.tmux.sh
+cp misc.sh ~/.misc.sh
 
 ${SEDI} \
   -e '/.git.sh/d' \
   -e '/.docker.sh/d' \
+  -e '/.misc.sh/d' \
   -e '/.tmux.sh/d' \
   ${RC}
 
 echo "source ~/.git.sh" >> ${RC}
 echo "source ~/.docker.sh" >> ${RC}
+echo "source ~/.misc.sh" >> ${RC}
 echo "source ~/.tmux.sh" >> ${RC}
 echo -e "\033[1mRun \`source ${RC}\` to apply new aliases!\033[0m"
---
+++ misc.sh
@@ -0,0 +1,58 @@
+unalias ? reload 2>/dev/null
+
+?() {
+  printf "c='clear'"\
+"\nbashrc='edit bashrc and reload'"\
+"\nzshrc='edit zshrc and reload'"\
+"\nreload='reload bashrc or zshrc'"\
+"\npc='toggle enable proxychains'"\
+"\n" | awk '{
+  sub(/\x27$/,"",$0);s=index($0,"="); printf "%6s = %s\n",
+  substr($0,0,s-1), substr($0,s+2)}' | sort -k 1,1 -b | \
+  column -c $(tput cols) | less -SFX
+}
+
+alias c='clear'
+
+alias bashrc='vim ~/.bashrc && source ~/.bashrc'
+alias zshrc='vim ~/.zshrc && source ~/.zshrc'
+      reload() {
+        [[ -e ~/.zshrc ]] && {
+          source ~/.zshrc
+        } || {
+          source ~/.bashrc
+        }
+      }
+
+# extend
+
+unalias grep 2>/dev/null
+alias grep='grep --exclude-dir={node_modules,bower_components,dist,.bzr,.cvs,.git,.hg,.svn,.tmp} --color=always'
+
+# proxychains
+
+unalias pc pcconf 2>/dev/null
+
+PC="proxychains"
+if hash proxychains4 2>/dev/null; then
+  PC="proxychains4 -q"
+fi
+
+pc() {
+  [[ $# -lt 1 ]] && {
+    [[ -e ~/.proxychains/proxychains.conf ]] && {
+      mv ~/.proxychains/proxychains.conf ~/.proxychains/proxychains.conf.disabled
+      echo proxychains disabled
+    } || {
+      mv ~/.proxychains/proxychains.conf.disabled ~/.proxychains/proxychains.conf
+      echo proxychains enabled
+    }
+  } || eval "$PC $@"
+}
+pcconf() {
+  [[ -e ~/.proxychains/proxychains.conf.disabled ]] && {
+    vim ~/.proxychains/proxychains.conf.disabled
+  } || {
+    vim ~/.proxychains/proxychains.conf
+  }
+}
