6a922c02dbec1b08f00f3dd5b148895fd74af388
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2015-01-01 17:11:49 +0800

    use install.sh shell script instead of Makefile
    because some OS like CoreOS doesn't have make installed

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ -e ~/.zshrc ]]; then
+  RC=~/.zshrc
+else
+  RC=~/.bashrc
+fi
+
+if [[ "`sed --version 2>&1`" == *"GNU"* ]]; then
+  SEDI='sed -i'
+else
+  SEDI='sed -i ""'
+fi
+
+cp git.sh ~/.git.sh
+cp docker.sh ~/.docker.sh
+cp tmux.sh ~/.tmux.sh
+
+${SEDI} \
+  -e '/.git.sh/d' \
+  -e '/.docker.sh/d' \
+  -e '/.tmux.sh/d' \
+  ${RC}
+
+echo "source ~/.git.sh" >> ${RC}
+echo "source ~/.docker.sh" >> ${RC}
+echo "source ~/.tmux.sh" >> ${RC}
+echo -e "\033[1mRun \`source ${RC}\` to apply new aliases!\033[0m"
